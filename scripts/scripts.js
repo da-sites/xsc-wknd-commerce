9de31d8a350b9c21f59e3d2dc4eee135c8f9d64a
Ryan Clayton <rgclayton@gmail.com>
2024-04-11 13:35:22 -0600

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -488,3 +488,13 @@ async function loadPage() {
 }
 
 loadPage();
+
+(async function daPreview() {
+  const { searchParams } = new URL(window.location.href);
+  const daPreview = searchParams.get('dapreview');
+  if (daPreview) {
+    const origin = daPreview === 'local' ? 'http://localhost:3000' : 'https://da.live';
+    const { default: livePreview } = await import(`${origin}/scripts/dapreview.js`);
+    livePreview(loadPage);
+  }
+}());
